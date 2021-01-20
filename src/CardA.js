0e6b9275b6f84e7e798d2f8152a90602128c52a8
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2021-01-20 21:44:15 +0530

    search results

--- src/CardA.js
+++ src/CardA.js
@@ -8,18 +8,20 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    width: 340,
+    maxWidth: 340,
+    height: 200
   },
   media: {
     height: 140,
   },
 });
 
-export default function CardA({content}) {
+export default function CardA({content,res}) {
   const classes = useStyles();
     return (
         <>
-    <Card  className={classes.root} variant="outlined">
+        {content ? 
+    <Card mb={4} className={classes.root} variant="outlined">
       <CardContent>
        {/*  className={classes.media} <img src="" className /> */}
         <Typography  color="Secondary" gutterBottom>
@@ -35,9 +37,27 @@ export default function CardA({content}) {
           well meaning and kindly.
         
         </Typography>
-      </CardContent>
+      </CardContent> 
       
-    </Card>
+    </Card> :   <Card mb={4} className={classes.root} variant="outlined">
+      <CardContent>
+       {/*  className={classes.media} <img src="" className /> */}
+        <Typography  color="Secondary" gutterBottom>
+         email:{res.email}
+        </Typography>
+        <Typography variant="h5" component="h2">
+       firstname:{res.first_name}
+        </Typography>
+        <Typography color="Secondary">
+         lastname:{res.last_name}
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+        
+        </Typography>
+      </CardContent> 
+      
+    </Card> }
         </>
     )
 }
--- src/Cards.js
+++ src/Cards.js
@@ -1,10 +1,14 @@
 import React, { useEffect,useState } from 'react'
 import Box from '@material-ui/core/Box'
-import { Card, CircularProgress, Grid } from '@material-ui/core';
+import { Card, CircularProgress, Grid ,InputBase } from '@material-ui/core';
+ 
 import CardA from './CardA'
 
 export default function Cards() {
-    const [ contents, setContent ] = useState()
+    const [ contents, setContent ] = useState();
+    const [ result, setResult ] = useState();
+    const [ search, setSearch ] = useState();
+    const [ Name, setName ] = useState()
 
     const getCardsdata = async () => {
         const response = await fetch("https://reqres.in/api/users?page=2");
@@ -18,14 +22,28 @@ export default function Cards() {
         getCardsdata()
     },[])
 
+    
+    const submit = (e) => {
+        e.preventDefault();
+        setSearch(Name)
+        let array = contents.filter((ans) => (
+            ans.first_name  == search
+        ))
+        console.log(array)
+          setResult(array)
+    }
     return (
         <div>
              <Box p={2} m={2} display="flex" justify="center"
               alignItems="center">
-             <h1>Cards</h1>
+           <h1>  Cards </h1>
+            <form className="searchform"   >
+                <input className="searchbar" value={Name} onChange={(e)=> setName(e.target.value)}  type="text"/>
+                <button  onClick={submit} className="search-btn" type="submit" >search</button>
+            </form>
              </Box>
              
-             <Grid container 
+             <Grid
               container
               direction="row"
               display="flex"
@@ -36,12 +54,36 @@ export default function Cards() {
 
               {  contents ? 
               contents.map((content) => (
-                  <CardA 
-                  key={content.id}
-                  content={content}/>
+                  <Grid item p={3} mb={3} xs={3} >
+                    <CardA 
+                    key={content.id}
+                    content={content}/>
+                  </Grid>
               )) : <CircularProgress /> }
+
             </Grid>
                 
+            <Grid container 
+            container
+            direction="column"
+            display="flex"
+            justify="center"
+            alignItems="center"
+            spacing={1}
+            m={3}
+            p={2}>
+             <h1>Search results</h1>
+
+             {  search ? 
+              result.map((res) => (
+                  <Grid item p={3} mb={3} xs={3} >
+                    <CardA 
+                    key={res.id}
+                    res={res}/>
+                  </Grid>
+              )) : <CircularProgress /> }
+             
+            </Grid>
            
         </div>
     )
